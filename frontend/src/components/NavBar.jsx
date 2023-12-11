0fa318d41851f6528738c44e4c9f209f095887e8
Laurore83 <laurore83@yahoo.fr>
2023-12-11 15:50:03 +0100

    navbar-mobile.v01

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-button">
+        <span className="material-symbols-outlined">home</span>
+        <p className="navbar-title">Accueil</p>
+      </div>
+      <div className="navbar-button">
+        <span className="material-symbols-outlined">map</span>
+        <p className="navbar-title">Carte</p>
+      </div>
+      <div className="navbar-button">
+        <span className="material-symbols-outlined">account_circle</span>
+        <p className="navbar-title">Compte</p>
+      </div>
+    </div>
+  );
+}
