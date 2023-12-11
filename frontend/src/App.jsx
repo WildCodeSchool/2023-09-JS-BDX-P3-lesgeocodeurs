1504b86d9446db8a2459da5c6dd5a29437b63c48
Laurore83 <laurore83@yahoo.fr>
2023-12-11 16:59:36 +0100

    navbar-route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
+      <Outlet />
       <NavBar />
     </div>
   );
