f225cda416912bb49547227cdac180a5b51ecadc
Antoine Cairey <cairey.antoine@gmail.com>
2023-12-13 15:35:48 +0100

    add unique modal on map

--- frontend/src/components/Station.jsx
+++ frontend/src/components/Station.jsx
@@ -1,32 +1,18 @@
-import { InfoWindow, Marker } from "@vis.gl/react-google-maps";
-import { useState } from "react";
+import { Marker } from "@vis.gl/react-google-maps";
 import PropTypes from "prop-types";
 
-export default function Station({ station }) {
-  const [open, setOpen] = useState(false);
-
+export default function Station({ station, setOpen, setSelectedStation }) {
   return (
-    <>
-      <Marker
-        position={{
-          lat: station.consolidated_latitude,
-          lng: station.consolidated_longitude,
-        }}
-        onClick={() => setOpen(true)}
-      />
-      {open && (
-        <InfoWindow
-          position={{
-            lat: station.consolidated_latitude,
-            lng: station.consolidated_longitude,
-          }}
-          onCloseClick={() => setOpen(false)}
-        >
-          <strong>{station.nom_station}</strong>
-          <p>{station.adresse_station}</p>
-        </InfoWindow>
-      )}
-    </>
+    <Marker
+      position={{
+        lat: station.consolidated_latitude,
+        lng: station.consolidated_longitude,
+      }}
+      onClick={() => {
+        setOpen(true);
+        setSelectedStation(station);
+      }}
+    />
   );
 }
 
@@ -34,7 +20,7 @@ Station.propTypes = {
   station: PropTypes.shape({
     consolidated_latitude: PropTypes.number.isRequired,
     consolidated_longitude: PropTypes.number.isRequired,
-    nom_station: PropTypes.string.isRequired,
-    adresse_station: PropTypes.string.isRequired,
   }).isRequired,
+  setOpen: PropTypes.func.isRequired,
+  setSelectedStation: PropTypes.func.isRequired,
 };
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,27 +1,40 @@
+import { useState } from "react";
 import { APIProvider, Map } from "@vis.gl/react-google-maps";
-import data from "../data-test.json";
 import Station from "../components/Station";
+import data from "../data-test.json";
 
 export default function MyMap() {
-  // const wild = { lat: 44.86943771320005, lng: -0.5651798933208533 };
   const position = { lat: 46.57829080854987, lng: 2.528225829979713 };
-  console.info(data);
+  const [open, setOpen] = useState(false);
+  const [selectedStation, setSelectedStation] = useState(null);
 
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-      <div style={{ height: "90vh" }}>
+      <div className="map">
         <Map
           zoom={5}
           center={position}
           gestureHandling="greedy"
           disableDefaultUI
+          onClick={() => setOpen(false)}
           /* mapId="baf145c89ada9afb" */
         >
           {data.map((station) => (
-            <Station key={station.id_station_itinerance} station={station} />
+            <Station
+              key={station.id_station_itinerance}
+              station={station}
+              setOpen={setOpen}
+              setSelectedStation={setSelectedStation}
+            />
           ))}
         </Map>
       </div>
+      {open && selectedStation && (
+        <div className="station-modal">
+          <strong>{selectedStation.nom_station}</strong>
+          <p>{selectedStation.adresse_station}</p>
+        </div>
+      )}
     </APIProvider>
   );
 }
