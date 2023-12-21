ffbd5c9e631709a4e2604489074a53a8d57845c4
Laurore83 <laurore83@yahoo.fr>
2023-12-21 14:14:20 +0100

    pull origin dev

--- frontend/src/pages/BackOfficeAccueil.jsx
+++ frontend/src/pages/BackOfficeAccueil.jsx
@@ -2,58 +2,44 @@ import {
   MDBContainer,
   MDBNavbar,
   MDBNavbarBrand,
-  MDBNavbarToggler,
   MDBNavbarNav,
   MDBNavbarItem,
   MDBNavbarLink,
-  MDBCollapse,
   MDBRow,
   MDBCard,
   MDBCol,
   MDBCardBody,
 } from "mdb-react-ui-kit";
-import { useState } from "react";
 
 export default function BAckOfficeAccueil() {
-  const [openNav, setOpenNav] = useState(false);
-
   return (
     <div className="backofficeaccueil_container">
       <h1>BackOffice Acceuil</h1>
-      <div>
-        <MDBNavbar expand="lg" light bgColor="light">
-          <MDBContainer fluid>
-            <MDBNavbarBrand className="title_back" href="#">
-              Back Office
-            </MDBNavbarBrand>
-            <MDBNavbarToggler
-              type="button"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-              onClick={() => setOpenNav(!openNav)}
-            />
-            <MDBCollapse navbar open={openNav}>
-              <MDBNavbarNav>
-                <MDBNavbarItem>
-                  <MDBNavbarLink
-                    active
-                    aria-current="page"
-                    href="/BackOfficeUtilisateur"
-                  >
-                    Utilisateurs
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href="#">Véhicules</MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href="#">Bornes</MDBNavbarLink>
-                </MDBNavbarItem>
-              </MDBNavbarNav>
-            </MDBCollapse>
-          </MDBContainer>
-        </MDBNavbar>
-      </div>
+      <MDBNavbar light bgColor="light">
+        <MDBContainer>
+          <MDBNavbarBrand className="title_back" href="#">
+            Back Office
+          </MDBNavbarBrand>
+
+          <MDBNavbarNav>
+            <MDBNavbarItem>
+              <MDBNavbarLink
+                active
+                aria-current="page"
+                href="/BackOfficeUtilisateur"
+              >
+                Utilisateurs
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#">Véhicules</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#">Bornes</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBContainer>
+      </MDBNavbar>
       <div className="dashboard_container">
         <MDBContainer fluid>
           <MDBRow className="justify-content-center">
