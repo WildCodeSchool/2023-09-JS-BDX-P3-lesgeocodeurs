4529e0194ad86aac4e1cf3289958508a206a9049
Laurore83 <laurore83@yahoo.fr>
2024-01-13 21:22:58 +0100

    backoffice utilisateur link to bdd

--- frontend/src/pages/BackOfficeUtilisateur.jsx
+++ frontend/src/pages/BackOfficeUtilisateur.jsx
@@ -1,117 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { MDBDatatable } from "mdb-react-ui-kit";
 import NavBarBackOffice from "../components/NavBarBackOffice";
 
 export default function BackOfficeUtilisateur() {
-  const basicData = {
-    columns: ["Id", "Email", "Prénom", "Nom", "DDN", "CP", "Ville"],
-    rows: [
-      [
-        "Tiger Nixon",
-        "System Architect",
-        "Edinburgh",
-        "61",
-        "2011/04/25",
-        "$320,800",
-      ],
-      [
-        "Garrett Winters",
-        "Accountant",
-        "Tokyo",
-        "63",
-        "2011/07/25",
-        "$170,750",
-      ],
-      [
-        "Ashton Cox",
-        "Junior Technical Author",
-        "San Francisco",
-        "66",
-        "2009/01/12",
-        "$86,000",
-      ],
-      [
-        "Cedric Kelly",
-        "Senior Javascript Developer",
-        "Edinburgh",
-        "22",
-        "2012/03/29",
-        "$433,060",
-      ],
-      ["Airi Satou", "Accountant", "Tokyo", "33", "2008/11/28", "$162,700"],
-      [
-        "Brielle Williamson",
-        "Integration Specialist",
-        "New York",
-        "61",
-        "2012/12/02",
-        "$372,000",
-      ],
-      [
-        "Herrod Chandler",
-        "Sales Assistant",
-        "San Francisco",
-        "59",
-        "2012/08/06",
-        "$137,500",
-      ],
-      [
-        "Rhona Davidson",
-        "Integration Specialist",
-        "Tokyo",
-        "55",
-        "2010/10/14",
-        "$327,900",
-      ],
-      [
-        "Colleen Hurst",
-        "Javascript Developer",
-        "San Francisco",
-        "39",
-        "2009/09/15",
-        "$205,500",
-      ],
-      [
-        "Sonya Frost",
-        "Software Engineer",
-        "Edinburgh",
-        "23",
-        "2008/12/13",
-        "$103,600",
-      ],
-      [
-        "Jena Gaines",
-        "Office Manager",
-        "London",
-        "30",
-        "2008/12/19",
-        "$90,560",
-      ],
-      [
-        "Quinn Flynn",
-        "Support Lead",
-        "Edinburgh",
-        "22",
-        "2013/03/03",
-        "$342,000",
-      ],
-      [
-        "Charde Marshall",
-        "Regional Director",
-        "San Francisco",
-        "36",
-        "2008/10/16",
-        "$470,600",
-      ],
-      [
-        "Haley Kennedy",
-        "Senior Marketing Designer",
-        "London",
-        "43",
-        "2012/12/18",
-        "$313,500",
-      ],
-    ],
-  };
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3310/api/users`);
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const columns = [
+    "id",
+    "email",
+    "first_name",
+    "last_name",
+    "birth_date",
+    "postal_code",
+    "city",
+  ];
+  const rows = userData.map((user) => [
+    user.id,
+    user.email,
+    user.first_name,
+    user.last_name,
+    user.birth_date,
+    user.postal_code,
+    user.city,
+  ]);
+
+  const basicData = { columns, rows };
   return (
     <div className="backofficeutilisateur_container">
       <h1>BackOffice Utilisateur</h1>
