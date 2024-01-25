e00d1c1a371147b1419388b7050091b95120b946
lulb11 <bordeslucas11@gmail.com>
2024-01-25 15:19:14 +0100

    test function servce

--- frontend/src/services/functions.service.jsx
+++ frontend/src/services/functions.service.jsx
@@ -10,6 +10,18 @@ class FunctionsService {
   static fetchStations = async () => {
     apiService.get(`${import.meta.env.VITE_BACKEND_URL}/api/station`);
   };
+
+  static returnAdmin = async () => {
+    try {
+      const res = await apiService.get(`http://localhost:3310/api/isadmin`);
+      if (res.message === "ok") {
+        return res.message;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    return null;
+  };
 }
 
 const functionsService = new FunctionsService();
