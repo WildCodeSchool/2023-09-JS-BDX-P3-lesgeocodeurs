a9f34dc76fb60d343688ce0ff306deb1d84bbc9b
Laurore83 <laurore83@yahoo.fr>
2024-01-26 16:10:38 +0100

    suppression cars utilisateur mise a jour

--- frontend/src/pages/Cars.jsx
+++ frontend/src/pages/Cars.jsx
@@ -47,11 +47,6 @@ export default function Cars() {
     }
   };
 
-  useEffect(() => {
-    fetchPlugTypes();
-    fetchData();
-  }, []);
-
   // Fonction pour ouvrir la boîte de dialogue de confirmation
   const openConfirmationDialog = (carId) => {
     setVehicleToDelete(carId);
@@ -84,6 +79,9 @@ export default function Cars() {
     cancelDeleteCar();
   };
 
+  useEffect(() => {
+    fetchPlugTypes();
+  }, []);
   function getPlugTypeName(plugTypeId) {
     const plugType = plugTypes.find((type) => type.id === plugTypeId);
     return plugType ? plugType.name : "Type inconnu";
@@ -101,7 +99,7 @@ export default function Cars() {
     textAlign: "center",
   });
   console.info(setDialogStyle);
-
+  fetchData();
   return (
     <div className="cars-container">
       <button type="submit" className="back" onClick={rtn}>
