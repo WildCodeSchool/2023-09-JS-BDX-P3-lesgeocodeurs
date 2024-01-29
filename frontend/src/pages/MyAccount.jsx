8d3b4d3936d4609b90024497f7f3f2f70fa59144
Laurore83 <laurore83@yahoo.fr>
2024-01-29 09:59:05 +0100

    css profil bouton supprimer ok

--- frontend/src/pages/MyAccount.jsx
+++ frontend/src/pages/MyAccount.jsx
@@ -3,9 +3,10 @@ import { MDBBtn } from "mdb-react-ui-kit";
 import { Link } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { useTheContext } from "../context/Context";
+import apiService from "../services/api.service";
 
 export default function MyAccount() {
-  const { logout, user, calculerAge, apiService } = useTheContext();
+  const { logout, user, calculerAge } = useTheContext();
 
   const userAge = calculerAge(
     user ? user.birth_date : "1995-01-01T00:00:00.000Z'"
@@ -25,7 +26,9 @@ export default function MyAccount() {
     const jwtToken = apiService.getToken();
     const token = jwtDecode(jwtToken);
     try {
-      await apiService.delete(`/users/${token.id}`);
+      await apiService.delete(
+        `${import.meta.env.VITE_BACKEND_URL}/api/users/${token.id}`
+      );
       // Mettre à jour l'état local ou recharger la liste de véhicules après la suppression
       // ...
       // Réinitialiser l'ID du véhicule à supprimer
@@ -33,9 +36,22 @@ export default function MyAccount() {
       setConfirmedDelete(true);
       // Fermer la boîte de dialogue après la suppression réussie
       logout();
+      alert("Votre compte a bien été supprimé");
     } catch (error) {
       console.error("Error deleting car:", error);
     }
+  };
+
+  // Position de la boîte de dialogue de confirmation
+  const dialogStyle = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "white",
+    padding: "20px",
+    zIndex: "1000",
+    textAlign: "center",
   };
 
   return (
@@ -64,7 +80,7 @@ export default function MyAccount() {
         </Link>
 
         <div className="modification-profil" />
-        <Link to={`/modifprofil/${user?.id}`}>
+        <Link to="/modifprofil">
           <MDBBtn className="buttonprofil" color="light" rippleColor="dark">
             <p>Modifier mon profil</p>
             <p>&rarr;</p>
@@ -91,35 +107,22 @@ export default function MyAccount() {
           </MDBBtn>
         </Link>
         <MDBBtn
-          size="sm"
           className="buttonprofil"
           color="light"
           rippleColor="dark"
           onClick={() => setShowConfirmation(true)}
         >
-          Supprimer
+          <p>Supprimer</p>
         </MDBBtn>
       </div>
       {/* Boîte de dialogue de confirmation */}
       {showConfirmation && (
-        <div
-          className="confirmation-dialog"
-          style={{
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            backgroundColor: "white",
-            padding: "20px",
-            zIndex: "1000",
-            textAlign: "center",
-          }}
-        >
+        <div className="confirmation-dialog" style={dialogStyle}>
           <p>Voulez-vous vraiment supprimer votre compte ?</p>
           <MDBBtn size="sm" onClick={confirmDeleteUser}>
             Oui
           </MDBBtn>
-          <MDBBtn size="sm" onClick={() => setShowConfirmation(false)}>
+          <MDBBtn size="sm" onClick={() => setShowConfirmation()}>
             Annuler
           </MDBBtn>
         </div>
